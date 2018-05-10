af0508af2bed9f4c0032f6e352eb02f3b1403adc
Celia Gearin <cgearin18@gmail.com>
2018-05-10 18:42:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten() {
+  kittens.slice(0, kittens.length - 1);
+}
